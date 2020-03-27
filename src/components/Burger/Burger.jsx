33300d7d21c7466c5d0adab1b1e3d185bf7cd829
viveloper <usik1234@gmail.com>
2020-03-27 10:57:09 +0900

    dynamic burger ingredients

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => {
+const Burger = props => {
+  const { ingredients } = props;
+  const transformedIngredients = Object.keys(ingredients).map(igKey => {
+    return Array(ingredients[igKey])
+      .fill('')
+      .map((_, index) => <BurgerIngredient key={igKey + index} type={igKey} />)
+      .reduce((arr, el) => arr.concat(el), []);
+  });
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 
 const layout = props => (
